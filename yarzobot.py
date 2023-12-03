c42e40e465923bb2f0a6ff567521c49154c75646
zlaham <zahi.lahham1@incrowdnow.com>
2023-12-04 00:44:37 +0200

    update data

--- yarzobot.py
+++ yarzobot.py
@@ -6,74 +6,70 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 from cryptography.fernet import Fernet
 
+# Define the main color from the logo as the background color
+background_color = "#98FB98"  # Light green background color
 
+# Streamlit page configuration
+st.set_page_config(page_title="YarzoBot", page_icon=":shamrock:")
+
+# Load encryption key and encrypted message
 with open("key.key", "rb") as key_file:
-    key =  key_file.read()
+    key = key_file.read()
 with open("encrypted.key", "rb") as encrypted_message:
-    encrypted_message =  encrypted_message.read()
+    encrypted_message = encrypted_message.read()
+
 fernet = Fernet(key)
 decrypted_message = fernet.decrypt(encrypted_message)
 OPENAI_API_KEY = decrypted_message.decode()
 
+# Custom CSS for the Streamlit app to match the logo theme
 st.markdown(
-    """
+    f"""
     <style>
-    .title {
-        color: white; /* Set text color to white */
+    .stApp {{
+        background-color: {background_color}; /* Light green background */
+    }}
+    
+    .title {{
+        color: black; /* Set title text color to black */
         font-size: 75px; /* Set font size */
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-st.markdown('<h1 class="title">YarzoBot</h1>', unsafe_allow_html=True)
-def add_bg_from_url():
-    st.markdown(
-         f"""
-         <style>
-         .stApp {{
-             background-image: url("https://reemsbucket.s3.us-west-2.amazonaws.com/background.jpeg");
-             background-attachment: fixed;
-             background-size: cover;
-             color: white;  /* Set text color to white */
-         }}
-         </style>  
-         """,
-         unsafe_allow_html=True
-     )
-add_bg_from_url()
-st.markdown(
-    """
-    <style>
-    .text-input-label {
-        color: white; /* Set text color to white */
+        text-align: center; /* Center the title */
+    }}
+    
+    .text-input-label {{
+        color: black; /* Set text color to black */
         margin-bottom: 0; /* Remove bottom margin */
         font-size: 18px; /* Set font size to 18 pixels */
-    }
+    }}
+
+    /* Style for the text input and responses */
+    .text-container {{
+        background-color: rgba(255, 255, 255, 0.8); /* Semi-transparent white */
+        padding: 10px;
+        margin-bottom: 10px;
+        border-radius: 5px; /* Rounded corners */
+        color: black; /* Text color black for readability */
+    }}
+
+    .stTextInput > input {{
+        color: black;
+    }}
+
+    .stButton > button {{
+        color: {background_color}; /* Green text color for buttons */
+        background-color: white; /* White background for buttons */
+        border: 2px solid white; /* White border for buttons */
+    }}
     </style>
     """,
     unsafe_allow_html=True
 )
 
-st_version = str(st.__version__)
-st.write('<p class="text-input-label">Tell me something...</p>', unsafe_allow_html=True)
+# Display the logo and title
+st.image('logo.png', width=300, use_column_width=True)  # Adjust path and width as needed
+st.markdown('<h1 class="title">YarzoBot</h1>', unsafe_allow_html=True)
 
-css = '''
-    <style>
-        .text-container {
-            background-color: rgba(255, 255, 255, 0.8);
-            padding: 10px;
-            margin-bottom: 10px;
-            color: black; /* Set the text color to black */
-        }
-
-        .text-container span {
-            color: black;
-        }
-    </style>
-'''
-
-st.markdown(css, unsafe_allow_html=True)
+# Initialize Pinecone
 try:
     index_name = 'alyarz-pinecone'
     text_field = "text"
@@ -101,11 +97,10 @@ try:
         chain_type="stuff",
         retriever=vectorstore.as_retriever()
     )
-
+    # Retrieve and display the chatbot response
     response = qa.run(query)
     if question:
-        st.markdown(f'<div class="text-container"><span>You:</span> {question}</div>', unsafe_allow_html=True)
-        st.markdown(f'<div class="text-container"><span>YarzoBot:</span> {response}</div>', unsafe_allow_html=True)
-
+        st.markdown(f'<div class="text-container">You: {question}</div>', unsafe_allow_html=True)
+        st.markdown(f'<div class="text-container">YarzoBot: {response}</div>', unsafe_allow_html=True)
 except Exception as e:
-    response = None
+    st.error(f"An error occurred: {e}")
